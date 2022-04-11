dd9216f7a2fc496a8bff627937aa77aaeafa9a5b
anderson_paschoalon <anderson_paschoalon@hst.com.br>
2022-04-11 13:26:16 -0300

    atualização no ordenandor

--- Gui/CsvReorderWindow.py
+++ Gui/CsvReorderWindow.py
@@ -1,12 +1,15 @@
 import csv
 import PySimpleGUI as sg
 from os.path import exists
+from PyUtils.CsvDicTuple import CsvDicTuple
 
 
 class CsvReorderWindow:
 
+    KEY_BUTTON_MOVE_UP = "key-btn-move-up"
+    KEY_BUTTON_MOVE_DOWN = "key-btn-move-down"
+
     def __init__(self):
-        print("todo")
         self.current_row_int = 0
 
 
@@ -17,6 +20,7 @@ class CsvReorderWindow:
         if not exists(filename):
             popup_text = "File {0} does not exist.".format(filename)
             return
+        dic_tuple = CsvDicTuple(dic_file=filename)
         data = self.load_csv(filename)
         table_headings = [label_key, label_value]
         layout_tools = []
@@ -35,7 +39,8 @@ class CsvReorderWindow:
                             expand_y=True,
                             enable_click_events=True,
                             tooltip='File {0}'.format(filename))],
-                        [sg.Button("/\ Move Up"), sg.Button("Move Down \/ ")]
+                        [sg.Button("/\ Move Up", key=CsvReorderWindow.KEY_BUTTON_MOVE_UP),
+                         sg.Button("Move Down \/ ", key=CsvReorderWindow.KEY_BUTTON_MOVE_DOWN )]
                     ]
         window = sg.Window(title="CsvReorderWindow [{0}]".format(filename),
                            layout=layout_table,
@@ -46,12 +51,28 @@ class CsvReorderWindow:
         while True:
             event, values = window.read(timeout=100000)
             curr_row = self.current_row(event, values)
-            # print("=> curr_row/self.current_row_int:" + str(curr_row) + "/" + str(self.current_row_int))
             if curr_row != self.current_row_int:
                 self.current_row_int = curr_row
                 print("=> curr_row:" + str(curr_row))
                 print("key:" + str(data[curr_row][0]) + ", value:" + str(data[curr_row][1]))
 
+            if event == sg.WIN_CLOSED:
+                print("Pressed button: sg.WIN_CLOSED")
+                break
+
+            if event == CsvReorderWindow.KEY_BUTTON_MOVE_UP:
+                key = data[self.current_row_int]
+                value = data[self.current_row_int]
+                dic_tuple.move_up(key, value)
+                data = self.load_csv(filename)
+                window['-TABLE-'].update(values=data[:][:])
+
+            if event == CsvReorderWindow.KEY_BUTTON_MOVE_DOWN:
+                key = data[self.current_row_int]
+                value = data[self.current_row_int]
+                dic_tuple.move_down(key, value)
+                data = self.load_csv(filename)
+                window['-TABLE-'].update(values=data[:][:])
 
     def load_csv(self, filename):
         data = []
@@ -74,7 +95,7 @@ class CsvReorderWindow:
 
 
 if __name__ == '__main__':
-    csv_file = "Actors.csv"
+    csv_file = "../Sandbox/DicTuple01.csv"
     reorder = CsvReorderWindow()
     data = reorder.load_csv(csv_file)
     print("data:" + str(data))
